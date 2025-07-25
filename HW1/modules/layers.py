e2a945bacaeaf0580e2c971153066eeb9f753978
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-07-25 17:39:20 +0300

    update

--- HW1/modules/layers.py
+++ HW1/modules/layers.py
@@ -123,16 +123,24 @@ class BatchNormalization(Module):
         if self.training:
             B = input.shape[0]
             self.mean = input.sum(axis=0) / B
-            self.var = np.power((input - self.mean), 2).sum(axis=0) / B
-            self.norm_input = (input - self.mean) / np.sqrt((self.var + self.eps))
+            self.input_mean = input - self.mean
+            self.var = np.power(self.input_mean, 2).sum(axis=0) / B
+            self.sqrt_var = np.sqrt((self.var + self.eps))
+            self.inv_sqrt_var = 1 / self.sqrt_var
+            self.norm_input = (self.input_mean) * self.inv_sqrt_var
             # update params
-            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * self.mean
-            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * B / (B - 1) * self.var
+            self.running_mean = (1 - self.momentum) * self.running_mean \
+                + self.momentum * self.mean
+            self.running_var = (1 - self.momentum) * self.running_var \
+                + self.momentum * B / (B - 1) * self.var
+            if self.affine:
+                self.norm_input = self.norm_input * self.weight + self.bias
+            return self.norm_input
         else:
-            self.norm_input = (input - self.running_mean) / np.sqrt(self.running_var + self.eps)
-        if self.affine:
-            return self.norm_input * self.weight + self.bias
-        return self.norm_input
+            norm_input_eval = (input - self.running_mean) * self.inv_sqrt_var
+            if self.affine:
+                norm_input_eval = norm_input_eval * self.weight + self.bias
+            return norm_input_eval
 
     def compute_grad_input(self, input: np.ndarray, grad_output: np.ndarray) -> np.ndarray:
         """
@@ -142,7 +150,16 @@ class BatchNormalization(Module):
         """
         # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
         # return super().compute_grad_input(input, grad_output)
-        return input
+        m = input.shape[0] # batch_size
+        dGdX = grad_output * self.weight if self.affine \
+            else grad_output # (batch_size, num_features)
+        dGdVar = (dGdX * self.input_mean * (-0.5) \
+            * np.power(self.var + self.eps, -1.5)).sum(axis=0) # (num_features,)
+        dGdMean = (dGdX * (-self.inv_sqrt_var)).sum(axis=0) \
+            + dGdVar * (-2 * self.input_mean).sum(axis=0) / m  # (num_features,)
+        dGdInput = dGdX * self.inv_sqrt_var + dGdVar * 2 * self.input_mean / m \
+            + dGdMean / m  # (batch_size, num_features) 
+        return dGdInput
 
     def update_grad_parameters(self, input: np.ndarray, grad_output: np.ndarray):
         """
@@ -151,8 +168,10 @@ class BatchNormalization(Module):
         """
         # replace with your code ｀、ヽ｀、ヽ(ノ＞＜)ノ ヽ｀☂｀、ヽ
         # super().update_grad_parameters(input, grad_output)
-        print("Hello")
-
+        if self.affine:
+            self.grad_weight += (grad_output * self.norm_input).sum(axis=0) # (num_features,) 
+            self.grad_bias += grad_output.sum(axis=0) # (num_features,)
+    
     def zero_grad(self):
         if self.affine:
             self.grad_weight.fill(0)
